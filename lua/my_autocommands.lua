a37ba0c539b84d84fde8526cfba77c4890f78d5b
Hawo Höfer <hawo.hoefer98@freenet.de>
2021-08-27 13:17:59 +0200

    update autocommands

--- lua/my_autocommands.lua
+++ lua/my_autocommands.lua
@@ -6,6 +6,7 @@ vim.api.nvim_exec('autocmd BufNewFile,BufRead *.c setlocal commentstring=//\\ %s
 vim.api.nvim_exec('autocmd BufNewFile,BufRead *.cpp setlocal commentstring=//\\ %s', true)
 vim.api.nvim_exec('autocmd BufNewFile,BufRead *.h setlocal commentstring=//\\ %s', true)
 vim.api.nvim_exec('autocmd BufNewFile,BufRead *.hpp setlocal commentstring=//\\ %s', true)
+vim.api.nvim_exec('autocmd BufNewFile,BufRead *.csv setlocal filetype=csv', true)
 
 -- Setting comment strings
 vim.api.nvim_exec('autocmd FileType python setlocal commentstring=#\\ %s', true)
@@ -21,6 +22,8 @@ vim.api.nvim_exec('autocmd FileType python nnoremap <leader>x :!python % <CR>', 
 vim.api.nvim_exec('autocmd FileType sh nnoremap <leader>x :w <CR>:! bash <<< cat %<CR>', true)
 vim.api.nvim_exec('autocmd FileType rust nnoremap <leader>x :w <CR>:! cargo run<CR>', true)
 
+-- Filetype Specific Commands
+vim.api.nvim_exec('autocmd FileType tex nnoremap <leader>B ciW\\textbf{<ESC>pa}<ESC>', true)
 -- Hold Cursor action
 vim.api.nvim_exec('autocmd CursorHold * :Lspsaga show_cursor_diagnostics', true)
 
@@ -29,7 +32,4 @@ vim.api.nvim_exec('autocmd Filetype python setlocal omnifunc=v:lua.vim.lsp.omnif
 
 vim.api.nvim_exec("autocmd FileType * hi trailws guibg='#fb4934'", true)
 vim.api.nvim_exec("autocmd FileType * match trailws /\\s\\+$/", true)
-vim.api.nvim_exec('autocmd FileType dashboard hi clear trailws', true)
-
-
 
