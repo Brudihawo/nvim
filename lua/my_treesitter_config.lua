fdfd7c84f5f10236cd02a5cfb5febd1f8775147c
Hawo Höfer <hawo.hoefer@kit.edu>
2024-08-06 11:01:41 +0200

    remove latex from treeshitter because of some bug

--- lua/my_treesitter_config.lua
+++ lua/my_treesitter_config.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "python", "c", "cpp", "bibtex", "json", "lua", "rust", "comment", "latex" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "python", "c", "cpp", "bibtex", "json", "lua", "rust", "comment" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = { "javascript" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
